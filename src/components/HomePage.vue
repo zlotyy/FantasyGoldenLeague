a8b52315d8c79fc43ec97a933f7e5d444f9c1afe
Arkadiusz Gajewski <arkadiusz.gajewski@pirios.com>
2018-11-22 23:27:37 +0100

    project start

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="content">
+    Home Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+  text-align: center;
+}
+</style>
